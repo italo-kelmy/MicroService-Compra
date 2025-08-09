f365931e9ff4eb4ee2e7bea8f4e557c5ca910e39
italo <italokelmy1122@hotmail.com>
2025-08-09 14:19:17 -0400

    adicionardo a configuração do rabbitmq

---
+++ src/main/java/com/github/com/italo_kelmy/compra_service/Configuration/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.github.com.italo_kelmy.compra_service.Configuration;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String FILA_COMPRA = "compra.fila";
+
+    @Bean
+    public Queue filaCompra() {
+        return new Queue(FILA_COMPRA, false);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
