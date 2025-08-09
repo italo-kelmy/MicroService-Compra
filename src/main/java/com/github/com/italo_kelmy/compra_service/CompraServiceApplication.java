09576999a21b49c66e48dd6e2c0a7f9d6abea541
italo <italokelmy1122@hotmail.com>
2025-08-09 14:17:47 -0400

    correção

--- src/main/java/com/github/com/italo_kelmy/compra_service/CompraServiceApplication.java
+++ src/main/java/com/github/com/italo_kelmy/compra_service/CompraServiceApplication.java
@@ -1,6 +1,7 @@
 package com.github.com.italo_kelmy.compra_service;
 
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableRabbit
 public class CompraServiceApplication {
 
     public static void main(String[] args) {
